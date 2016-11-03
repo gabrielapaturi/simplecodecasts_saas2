8d44427b622bc35e0954917fdf4cf1dffc82c60a
gabrielapaturi <gabriela.lopes@icloud.com>
2016-11-03 23:24:25 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,3 @@ class ProfilesController < ApplicationController
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 end
-
-
-end
